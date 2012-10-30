94184341a0c772d2f2582f3f1c5c459f4faa2bf0
Sebastian Röbke <sebastian.roebke@xing.com>
2012-10-30 16:54:23 +0100

    Add integration test to verify deletion works

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -8,13 +8,25 @@ describe "Integration", :integration => true do
     FtpServer.start
   end
 
+  let(:file) { File.new(File.expand_path("../support/integration/avatar.jpg", __FILE__), "rb") }
+
   it "stores the attachment on the ftp servers" do
-    file = File.new(File.expand_path("../support/integration/avatar.jpg", __FILE__), "rb")
     user = User.new
     user.avatar = file
     user.save!
     file.close
 
     File.exists?(FtpServer::HOME_PATH + "/#{user.id}/original/avatar.jpg").should be_true
+  end
+
+  it "deletes an attachment from the ftp servers" do
+    user = User.new
+    user.avatar = file
+    user.save!
+    file.close
+
+    user.destroy
+
+    File.exists?(FtpServer::HOME_PATH + "/#{user.id}/original/avatar.jpg").should be_false
   end
 end
