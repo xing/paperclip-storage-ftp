38a4f03884b306fe1a8d533d7ce9047c227ff3d9
Sebastian Röbke <sebastian.roebke@xing.com>
2014-02-05 18:20:30 +0100

    Try to fix travis with increased log_file_activity_timeout

--- spec/support/integration/ftp_server.rb
+++ spec/support/integration/ftp_server.rb
@@ -23,12 +23,13 @@ class FtpServer
 
   def self.daemon_controller
     @daemon_controller ||= DaemonController.new(
-      :identifier     => "Apache FtpServer",
-      :start_command  => "cd #{INSTALL_PATH}; ./bin/ftpd.sh res/conf/ftpd-typical.xml",
-      :ping_command   => [:tcp, '127.0.0.1', 2121],
-      :pid_file       => INSTALL_PATH + "/res/ftpd.pid",
-      :log_file       => INSTALL_PATH + "/res/log/ftpd.log",
-      :start_timeout  => ENV['TRAVIS'] ? 120 : 10
+      :identifier                => "Apache FtpServer",
+      :start_command             => "cd #{INSTALL_PATH}; ./bin/ftpd.sh res/conf/ftpd-typical.xml",
+      :ping_command              => [:tcp, '127.0.0.1', 2121],
+      :pid_file                  => INSTALL_PATH + "/res/ftpd.pid",
+      :log_file                  => INSTALL_PATH + "/res/log/ftpd.log",
+      :start_timeout             => ENV['TRAVIS'] ? 120 : 10,
+      :log_file_activity_timeout => ENV['TRAVIS'] ? 120 : 10
     )
   end
 end
